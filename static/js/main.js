459e1e6ad268a2604eda41c575a0f1186e4bd75d
Mohamed EL-Khouly <msirag95@gmail.com>
2021-04-22 20:27:01 +0200

    under mentainence

--- static/js/main.js
+++ static/js/main.js
@@ -15,69 +15,15 @@
  *
 */
 
-const resultDisplay = document.querySelector('.display'),
-	calcBtn = document.querySelectorAll('[data-value]');
 
-let result = 0,
-	operator = '',
-	op1 = 0,
-	op2 = 0;
+
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
 
-// 1. display results
-function displayResult() {
-	resultDisplay.textContent = result;
-}
 
-// 2. click button action
-function clickedBtn() {
-	const val = this.dataset.value;
-	switch (val) {
-		case 'clear':
-			result = 0;
-			break;
-		case 'add':
-			operator = '+';
-			op1 = result;
-			result = 0;
-			break;
-		case 'subtract':
-			operator = '-';
-			op1 = result;
-			result = 0;
-			break;
-		case 'multiply':
-			operator = '*';
-			op1 = result;
-			result = 0;
-			break;
-		case 'divide':
-			operator = '/';
-			op1 = result;
-			result = 0;
-			break;
-		case 'modules':
-			operator = '%';
-			op1 = result;
-			result = 0;
-			break;
-		case 'equal':
-			op2 = result;
-			result = eval(op1 + operator + op2);
-			break;
-		case 'negative':
-			result = eval('-' + result);
-			break;
-		default:
-			result === 0 ? result = val : result += val;
-			break;
-	}
-	displayResult();
-}
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -90,6 +36,3 @@ function clickedBtn() {
  *
 */
 
-calcBtn.forEach(btn => {
-	btn.addEventListener('click', clickedBtn);
-});
